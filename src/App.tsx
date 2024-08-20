2a8789438e3312b4750a922c4fe55281f00a54f1
Bartłomiej Marzec <bmarzec@sii.pl>
2024-08-20 18:32:49 +0200

    upgraded the header

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
         <h1 className="text-xl font-bold text-center">
-          🚀 Rick and Morty - Fan Service - {__APP_VERSION__}
+          🚀 Rick and Morty - Fan Service - {__APP_VERSION__} 🚀
         </h1>
       </div>
       <Outlet />
