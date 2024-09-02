dcc823b4f422d7886601c89282ca0015ac431c00
Bartłomiej Marzec <bmarzec@sii.pl>
2024-09-02 21:20:05 +0200

    pull the s3 and cloudfront config from aws

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,17 +1,8 @@
-resource "aws_s3_bucket" "ofe_test_bucket" {
-  bucket = "ofe-test-bucket-3258"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket_website_configuration" "ofe_test_bucket_website_configuration" {
-  bucket = aws_s3_bucket.ofe_test_bucket.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
+resource "aws_s3_bucket" "rm_cicd" {
+  bucket              = "rm-cicd"
+  bucket_prefix       = null
+  force_destroy       = null
+  object_lock_enabled = false
+  tags                = {}
+  tags_all            = {}
 }
