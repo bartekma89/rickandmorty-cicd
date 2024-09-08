110d1d2fbc9cee63197593636f30002880c54e19
Bartłomiej Marzec <bmarzec@sii.pl>
2024-09-09 00:23:35 +0200

    added s3 bucket

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,26 @@ import { Outlet } from 'react-router-dom';
 const App = () => {
   return (
     <div>
+      <header>
+        <img
+          src={`${import.meta.env.VITE_S3_IMAGES_BUCKET}/header.png`}
+          alt="header logo"
+          className="mb-4"
+        />
+      </header>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
         <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - From AWS! 🚀</h1>
       </div>
       <Outlet />
       <footer>
-        <div className="flex">
+        <div className="flex relative">
           <img
-            src="/resources/img/logo-dark-vertical-1.png"
+            src={`${import.meta.env.VITE_S3_IMAGES_BUCKET}/logo-dark-vertical-1.png`}
             alt="Logo projektu Opanuj Frontend"
             className="w-32 mt-4 mx-auto justify-center"
           />
           {import.meta.env.VITE_ENVIRONMENT !== 'PROD' && (
-            <div className="justify-end self-center bg-white rounded-lg shadow-sm p-2">
+            <div className="self-center absolute right-0 bg-white rounded-lg shadow-sm p-2">
               ENV: {import.meta.env.VITE_ENVIRONMENT ?? 'Local'} - VER: {__APP_VERSION__}
             </div>
           )}
--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly APP_VERSION: string;
   readonly VITE_ENVIRONMENT: string;
+  readonly VITE_S3_IMAGES_BUCKET: string;
 }
 
 interface ImportMeta {
